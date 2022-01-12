a3d80a08877cd3319546962b60f5d4712b2cff8e
4matt <4man.matthew@gmail.com>
2022-01-11 19:33:43 -0600

    Added getters in Blockchain and Transaction
    - 4mattX

--- Blockchain.py
+++ Blockchain.py
@@ -2,3 +2,8 @@ class Blockchain (object):
     def __init__(self):
         self.chain = []
 
+    def getLastBlock(self):
+        return self.chain[-1]
+
+    def addBlock(self, block):
+        self.chain.append(block)
