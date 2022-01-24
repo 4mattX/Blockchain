79d3da14e374ab406b051b70d7c438c608c131c8
Thuan <msu-tle15@mcneese.edu>
2022-01-24 17:34:46 -0600

    started on the blockchain api using fast api as the base

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+#To test api, go to terminal and type in "pip install fastapi" and then "pip install uvicorn"
 from fastapi import FastAPI, Path
 import uvicorn
 from typing import Optional
@@ -5,28 +6,31 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class user(BaseModel):
-    userID: int
+#data structures
+class userID(BaseModel):
     userName: str
     balance: Optional[float] = 0.00
 
-usersList = {}
 
+registeredUsers = {}
+
+#default loading page
 @app.get("/")
 def home():
     return{"Home Page"}
 
 #functions for the users class
-@app.post("/create_user/{userID}/{userName}/{balance}")
-def create_user(userID: int, userName: str, balance: float = 0.00):
-    if user.userID in usersList:
-        return {"Error": "User ID has already been taken."}
-    elif user.userName in usersList:
-        return {"Error": "User name has already been taken."}
+@app.post("/create_user/{userID}")
+def create_user(userID: int, item: userID):
+    if userID in registeredUsers:
+        return {"Error": "User ID already taken."}
+
+    registeredUsers[userID] = {"userName": item.userName, "balance": item.balance}
+    return registeredUsers[userID]
 
 @app.get("/get_user/{userID}")
-def get_user(userID: int, item: user):
-    return usersList[userID]
+def get_user(userID: int):
+    return registeredUsers[userID]
 
 
 
@@ -34,3 +38,47 @@ def get_user(userID: int, item: user):
 
 
 uvicorn.run(app)
+#
+# from fastapi import FastAPI, Path
+# import uvicorn
+# from typing import Optional
+# from pydantic import BaseModel
+#
+# app = FastAPI()
+#
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     brand: Optional[str] = None
+#
+# inventory = {
+#     1: {
+#         "name": "Milk",
+#         "price": "3.99",
+#         "brand": "regular"
+#     }
+# }
+#
+# @app.get("/")
+# def home():
+#     return {"Home Page"}
+#
+# @app.get("/about/")
+# def about():
+#     return {"Data": "About"}
+#
+# @app.get("/get_item/{itemID}")
+# def get_item(itemID: int):
+#     return inventory[itemID]
+#
+# @app.post("/create_item/{itemID}")
+# def create_item (itemID: int, item: Item):
+#     if itemID in inventory:
+#         return {"Error": "Item ID already exists"}
+#
+#     inventory[itemID] = {"name": item.name, "price": item.price, "brand": item.brand,}
+#     return inventory[itemID]
+#
+#
+#
+# uvicorn.run(app)
